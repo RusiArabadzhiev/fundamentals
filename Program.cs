d0ebfc408d9a9f7519a11f4302218402206f0152
RusiArabadzhiev <111768090+RusiArabadzhiev@users.noreply.github.com>
2023-08-21 13:10:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace currentoddnumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            int i = 0;
+            bool hasenoughnumbers = true;
+
+            while (hasenoughnumbers)
+            {
+
+                int currentodnumber = 1 + (i * 2);
+                sum += currentodnumber;
+                i++;
+                Console.WriteLine($"{currentodnumber}");
+
+
+                if (i == number) { 
+                
+                    hasenoughnumbers=false;
+                }
+
+                
+
+            }
+
+
+        }
+    }
+}
